40191601b487d88a9974b2dc953d7e7c64e1928e
o0Manavaul0o <87138886+o0Manavaul0o@users.noreply.github.com>
2021-07-23 11:12:21 -0500

    Initial Commit

---
+++ Manav_FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manav_FoodApp
+//
+//  Created by Student42 on 7/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
