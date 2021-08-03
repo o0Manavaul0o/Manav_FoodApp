a485de3d2f77585b2c6082bf4e0eb673d052c4dd
o0Manavaul0o <87138886+o0Manavaul0o@users.noreply.github.com>
2021-08-03 13:04:47 -0500

    foodapp featured page (kinda works)

---
+++ Manav_FoodApp/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  Manav_FoodApp
+//
+//  Created by Student42 on 7/23/21.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
